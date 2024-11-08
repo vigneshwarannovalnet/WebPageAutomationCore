9625cb46380c9f823d8bf4f0f9d574bf19a12f3d
mohamedn_m <mohamedn_m@novalnetsolutions.com>
2024-11-08 11:06:29 +0530

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,25 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
-        ChromeDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        ChromeDriver driver = new ChromeDriver(options);
         driver.get("https://github.com/");
+        driver.findElement(By.linkText("Sign in")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//h1"))));
+        System.out.println(element.getText());
         driver.quit();
     }
 }
