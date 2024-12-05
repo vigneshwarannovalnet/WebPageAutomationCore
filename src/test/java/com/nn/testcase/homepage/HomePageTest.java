2e2605b2e888cbc2d42ac30a55cc3d5a8990997e
mohamedn_m <mohamedn_m@novalnetsolutions.com>
2024-12-05 19:29:33 +0530

    Uncommanded

--- src/test/java/com/nn/testcase/homepage/HomePageTest.java
+++ src/test/java/com/nn/testcase/homepage/HomePageTest.java
@@ -10,28 +10,28 @@ public class HomePageTest extends BaseTest {
     public void brokenLink_de() throws GeneralSecurityException, IOException {
        System.out.println("Starter: brokenLink_de: " + Thread.currentThread().getName());
        System.out.println("Thread ID: " + Thread.currentThread().getId());
-       //verifyCompleteLinks("https://www.novalnet.de/sitemap_index.xml", "DE");
+       verifyCompleteLinks("https://www.novalnet.de/sitemap_index.xml", "DE");
     }
 
     @Test(priority = 2, description = "Check whether the broken links are present or not in the homePage en site")
     public void brokenLink_en() throws GeneralSecurityException, IOException {
         System.out.println("Starter: brokenLink_en: " + Thread.currentThread().getName());
         System.out.println("Thread ID: " + Thread.currentThread().getId());
-     // verifyCompleteLinks("https://www.novalnet.com/sitemap_index.xml", "EN");
+     verifyCompleteLinks("https://www.novalnet.com/sitemap_index.xml", "EN");
     }
 
    @Test(priority = 3,description = "Check whether the broken image are present or not in the homePage de site")
     public void brokenImage_DE() throws GeneralSecurityException, IOException {
        System.out.println("Starter: brokenImage_DE: " + Thread.currentThread().getName());
        System.out.println("Thread ID: " + Thread.currentThread().getId());
-         //verifyBrokenImages("https://www.novalnet.de/sitemap_index.xml","DE");
+         verifyBrokenImages("https://www.novalnet.de/sitemap_index.xml","DE");
     }
 
     @Test(priority = 4,description = "Check whether the broken image are present or not in the homePage en site")
     public void brokenImage_EN() throws GeneralSecurityException, IOException {
         System.out.println("Starter: brokenImage_EN: " + Thread.currentThread().getName());
         System.out.println("Thread ID: " + Thread.currentThread().getId());
-      // verifyBrokenImages("https://www.novalnet.com/sitemap_index.xml","DE");
+      verifyBrokenImages("https://www.novalnet.com/sitemap_index.xml","DE");
     }
 
 }
