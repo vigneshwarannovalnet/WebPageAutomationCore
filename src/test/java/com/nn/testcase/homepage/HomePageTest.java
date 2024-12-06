830346dc1641442c4dc4ff3d4b873d213aea5e19
mohamedn_m <mohamedn_m@novalnetsolutions.com>
2024-12-06 16:51:42 +0530

    Test

--- src/test/java/com/nn/testcase/homepage/HomePageTest.java
+++ src/test/java/com/nn/testcase/homepage/HomePageTest.java
@@ -11,39 +11,33 @@ import java.util.Arrays;
 public class HomePageTest extends BaseTest {
     private static File DE_xl = new File(System.getProperty("user.dir") + "/src/test/resources/DE_HomePage.xlsx");
 
-    //@Test(priority = 1, description = "Check whether the broken links  are present or not in the homePage de site")
+    @Test(priority = 1, description = "Check whether the broken links  are present or not in the homePage de site")
     public void brokenLink_de() throws GeneralSecurityException, IOException {
        System.out.println("Starter: brokenLink_de: " + Thread.currentThread().getName());
        System.out.println("Thread ID: " + Thread.currentThread().getId());
        verifyCompleteLinks("https://www.novalnet.de/sitemap_index.xml", "DE");
     }
 
-   // @Test(priority = 2, description = "Check whether the broken links are present or not in the homePage en site")
+   @Test(priority = 2, description = "Check whether the broken links are present or not in the homePage en site")
     public void brokenLink_en() throws GeneralSecurityException, IOException {
         System.out.println("Starter: brokenLink_en: " + Thread.currentThread().getName());
         System.out.println("Thread ID: " + Thread.currentThread().getId());
      verifyCompleteLinks("https://www.novalnet.com/sitemap_index.xml", "EN");
     }
 
-   //@Test(priority = 3,description = "Check whether the broken image are present or not in the homePage de site")
+   @Test(priority = 3,description = "Check whether the broken image are present or not in the homePage de site")
     public void brokenImage_DE() throws GeneralSecurityException, IOException {
        System.out.println("Starter: brokenImage_DE: " + Thread.currentThread().getName());
        System.out.println("Thread ID: " + Thread.currentThread().getId());
          verifyBrokenImages("https://www.novalnet.de/sitemap_index.xml","DE");
     }
 
-  //@Test(priority = 4,description = "Check whether the broken image are present or not in the homePage en site")
+   @Test(priority = 4,description = "Check whether the broken image are present or not in the homePage en site")
     public void brokenImage_EN() throws GeneralSecurityException, IOException {
         System.out.println("Starter: brokenImage_EN: " + Thread.currentThread().getName());
         System.out.println("Thread ID: " + Thread.currentThread().getId());
       verifyBrokenImages("https://www.novalnet.com/sitemap_index.xml","DE");
     }
 
-    @Test
-    public void test() throws IOException {
-        System.out.println("File path is: " + DE_xl.getAbsolutePath());
-        writeDataToSheet_DE("nizam", new ArrayList<Object>(Arrays.asList("url", "statusCode", "statusMessage", "sourceUrl")), DE_xl);
-
-    }
 
 }
