d3a99dedd5183a353c7f3c959b698715cc484612
mohamedn_m <mohamedn_m@novalnetsolutions.com>
2024-12-10 16:36:02 +0530

    Test

--- src/test/java/com/nn/testcase/homepage/HomePageTest.java
+++ src/test/java/com/nn/testcase/homepage/HomePageTest.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 public class HomePageTest extends BaseTest {
 
 
-   @Test(priority = 1,description = "Check whether the broken links  are present or not in the homePage de site")
+   //@Test(priority = 1,description = "Check whether the broken links  are present or not in the homePage de site")
     public void brokenLink_de() throws GeneralSecurityException, IOException {
        verifyCompleteLinks("https://www.novalnet.de/sitemap_index.xml","DE");
     }
@@ -23,12 +23,12 @@ public class HomePageTest extends BaseTest {
       verifyCompleteLinks("https://www.novalnet.com/sitemap_index.xml","EN");
     }
 
-     @Test(priority = 3,description = "Check whether the broken image are present or not in the homePage de site")
+    // @Test(priority = 3,description = "Check whether the broken image are present or not in the homePage de site")
     public void brokenImage_DE() throws GeneralSecurityException, IOException {
        verifyBrokenImages("https://www.novalnet.de/sitemap_index.xml","DE");
     }
 
-     @Test(priority = 4,description = "Check whether the broken image are present or not in the homePage en site")
+    // @Test(priority = 4,description = "Check whether the broken image are present or not in the homePage en site")
     public void brokenImage_EN() throws GeneralSecurityException, IOException {
        verifyBrokenImages("https://www.novalnet.com/sitemap_index.xml","EN");
     }
